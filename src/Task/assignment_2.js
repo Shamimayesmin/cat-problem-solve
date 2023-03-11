5d4c18940c1a1a2ea8cd57923f819f8d5491bb40
shamima <shamimayesmin144@gamil.com>
2023-03-11 19:44:15 +0600

    done

--- src/Task/assignment_2.js
+++ src/Task/assignment_2.js
@@ -1,19 +1,4 @@
 // for 2019
-const all_skills = [
-	"Agility and Coordination",
-	"Hunting and Predatory Skills",
-	"Communication",
-	"Adaptability",
-	"Independence",
-	"Affection and Socialization",
-	"Climbing",
-	"Balance",
-	"Senses",
-	"Flexibility",
-	"Problem-solving",
-	"Stealth",
-];
-
 const range_2019 = {
 	Agility_Coordination: 64,
 	Hunting_Skills: 76,
@@ -48,6 +33,7 @@ const range_2024 = {
 const today_2019_sorted=[];
 const bottom3_2019_sort=[]
 
+
 // array of sorted top and bottom value in 2024
 const future_2024_sorted= [];
 const bottom3_2024_sorted= [];
@@ -73,7 +59,8 @@ function Sort_Bottom(value){
     })
 }
 
-// for 1019
+
+
 To_Add(range_2019, today_2019_sorted)
 To_Add(range_2019, bottom3_2019_sort)
 Sort_Object(today_2019_sorted)
@@ -86,23 +73,46 @@ To_Add(range_2024,bottom3_2024_sorted)
 Sort_Object(future_2024_sorted)
 Sort_Bottom(bottom3_2024_sorted)
 
-// console.log("for -2019 top", today_2019_sorted);
-// console.log("for -2019 bottom", bottom3_2019_sort);
-// console.log("for -2024 top", future_2024_sorted);
-// console.log("for -2024 bottom", bottom3_2024_sorted);
+console.log("for -2019 top", today_2019_sorted);
+console.log("for -2019 bottom", bottom3_2019_sort);
+console.log("for -2024 top", future_2024_sorted);
+console.log("for -2024 bottom", bottom3_2024_sorted);
 
 const top3_2019 = [today_2019_sorted.slice(0,3)]
 const botton3_2019 = [bottom3_2019_sort.slice(0,3)]
 const top3_2024 = [future_2024_sorted.slice(0,3)]
 const bottom3_2024 = [bottom3_2024_sorted.slice(0,3)]
 
-const sum = top3_2019+top3_2024
-console.log("result" , sum);
-
-
 console.log("top",top3_2019);
 console.log("bottom",botton3_2019);
 
 console.log("top",top3_2024);
 console.log( 'bottom',bottom3_2024);
+
+
+const all_skills = [
+	{name:"Agility and Coordination", score2019: 64, score2024: 71},
+	{name:"Hunting and Predatory Skills",score2019: 76, score2024: 70},
+	{name:"Communication",score2019: 60, score2024: 75},
+	{name:"Adaptability",score2019: 82, score2024: 63},
+	{name:"Independence",score2019: 67, score2024: 78},
+    { name: 'Affection and Socialization', score2019: 70, score2024: 84 },
+	{name:"Climbing",score2019: 90, score2024: 95},
+	{name:"Balance",score2019: 88, score2024: 96},
+	{name:"Senses",score2019: 86, score2024: 90},
+	{name:"Flexibility",score2019: 75, score2024: 83},
+	{name:"Problem-solving",score2019: 67, score2024: 78},
+	{name:"Stealth",score2019: 59, score2024: 72},
+];
+
+const sortedCapabilities = all_skills.sort((a, b) => (b.score2024 - b.score2019) - (a.score2024 - a.score2019));
+
+//  top-3 accelerating
+console.log('Top-3 accelerating capabilities:');
+for (let i = 0; i < 3; i++) {
+    console.log((sortedCapabilities[i].name),(sortedCapabilities[i].score2024 - sortedCapabilities[i].score2019));
+};
+
+
+
 
